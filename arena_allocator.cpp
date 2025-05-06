954a9375481c4df2b652cf008347d73ef4f96e45
Iago Caldentey Palmer <icaldenteypalmer@gmail.com>
2025-05-06 11:14:59 +0200

    Initial commit

---
+++ arena_allocator.cpp
@@ -0,0 +1,86 @@
+#include "arena_allocator.h"
+
+#include <cstdlib>
+
+void* arena_alloc(Arena* arena, size_t size_bytes)
+{
+    size_t size = (size_bytes + sizeof(uintptr_t) - 1) / sizeof(uintptr_t);
+    Region* current = arena->begin;
+
+    // Find first free region with enough space to allocate the new size
+    while(current != nullptr && current->capacity - current->occupied < size)
+    {
+        current = current->next;
+    }
+
+    if(current == nullptr)
+    {
+        // No region found, new must be created
+        current = new_region(size_bytes);
+        if(arena->end != nullptr)
+        {
+            arena->end->next = current;
+        }
+        arena->end = current;
+
+        if(arena->begin == nullptr)
+        {
+            arena->begin = current;
+        }
+    }
+    void* data = &current->data[current->occupied];
+    current->occupied += size;
+
+    return data;
+}
+
+Region* new_region(size_t size_bytes)
+{
+    size_t final_size_bytes = std::max(size_bytes, (size_t)REGION_DATA_SIZE);
+    size_t qword_count = (final_size_bytes + sizeof(uintptr_t) - 1) / sizeof(uintptr_t);
+    size_t size_bytes_total = sizeof(Region) + (qword_count * sizeof(uintptr_t));
+
+    Region* r = (Region*)malloc(size_bytes_total);
+    assert(r);
+    r->capacity = qword_count;
+    r->occupied = 0;
+    r->next = nullptr;
+
+    return r;
+}
+
+void region_reset(Region* r)
+{
+    r->occupied = 0;
+}
+
+void arena_reset(Arena* arena)
+{
+    Region* r = arena->begin;
+    while(r)
+    {
+        region_reset(r);
+        r = r->next;
+    }
+}
+
+void free_arena(Arena* arena)
+{
+    Region* r = arena->begin;
+    while(r)
+    {
+        Region* c = r;
+        r = r->next;
+        free(c);
+    }
+
+    arena->begin = nullptr;
+    arena->end = nullptr;
+}
+
+void* arena_memdup(Arena* arena, const void* src, size_t size_bytes)
+{
+    void* dst = arena_alloc(arena, size_bytes);
+    memcpy(dst, src, size_bytes);
+    return dst;
+}
---
+++ arena_allocator.h
@@ -0,0 +1,74 @@
+#ifndef ARENA_H
+#define ARENA_H
+
+#include <cstddef>
+#include <cstdint>
+
+/*
+ * Example usage:
+ *
+ * Arena arena = {0};   // Create arena
+ *
+ * V2i* v1 = (V2i*)arena_alloc(&arena, sizeof(V2i));        // Store a V2i
+ * v1->x = 1;                                               // Set value to its X component
+ * or
+ * V2i* v1 = new(arena_alloc(&arena, sizeof(V2i)))V2i(1,0); // Store and initialize with constructor a V2i
+ *
+ *  // Now v1 can be used as any other V2i
+ *  // You can duplicate it into another V2i
+ *  V2i* v2 = arena_memdup(&arena, v1, sizeof(V2i));
+ *
+ *  // Once the contents of the arena are no longer needed:
+ *  arena_reset(&arena);
+ *
+ *  // Once the arena itself is no longer needed, destroy all its regions.
+ *  // Note that it not frees the arena itself, only its regions.
+ *  free_arena(&arena);
+ *
+ */
+
+// By default a region's data block occupies 8KB. Define if different size is needed
+// If an allocation of a bigger object is done, a region with that size will be created instead
+#ifndef REGION_DATA_SIZE
+  #define REGION_DATA_SIZE 8*1024
+#endif
+
+#define cast_ptr(ptr) (decltype(ptr))
+
+typedef struct Region Region;
+
+struct Region{
+    Region* next;
+    size_t capacity;
+    size_t occupied;
+    uintptr_t data[];
+};
+
+typedef struct {
+    Region* begin;
+    Region* end;
+} Arena;
+
+// ------------------------------------
+// Region code
+//  ------------------------------------
+
+Region* new_region(size_t size);
+
+void region_reset(Region* r);
+
+// ------------------------------------
+// Arena code
+//  ------------------------------------
+
+void* arena_alloc(Arena* arena, size_t size_bytes);
+
+// Resets the regions, it does not free them.
+void arena_reset(Arena* arena);
+
+// Frees all the regions of the arena. NOT THE ARENA ITSELF!!
+void free_arena(Arena* arena);
+
+void* arena_memdup(Arena* arena, const void* src, size_t size_bytes);
+
+#endif // ARENA_H
